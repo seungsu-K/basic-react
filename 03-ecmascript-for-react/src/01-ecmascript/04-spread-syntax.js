18a8179290605a8d574bc40e80d3c495d1a38523
seungsu-K <kss095@naver.com>
2024-07-22 16:42:01 +0900

    [학습] Spread-syntax
    - 전개 구문

--- 03-ecmascript-for-react/src/01-ecmascript/04-spread-syntax.js
+++ 03-ecmascript-for-react/src/01-ecmascript/04-spread-syntax.js
@@ -1,13 +1,18 @@
 // --------------------------------------------------------------------------
 // spread syntax
-// - 전개 구문을 사용해 배열 병합
-// - 전개 구문을 사용해 객체 병합
+// - 전개 구문을 사용해 배열 병합 (합성, 복사해서 결합)
+// - 전개 구문을 사용해 객체 병합 (합성, 복사해서 결합)
 // --------------------------------------------------------------------------
 
 function combineArray() {
   const numberList = [2, -2, 1];
   const countList = [101, 201, 301];
 
+  // 두 배열을 하나로 병합하려면?
+  const nList = numberList.concat(countList);
+  console.log(nList);
+
+  // 배열을 자르고 병합하려면?
   const combineList = countList
     .slice(0, 2)
     .concat(numberList)
@@ -17,7 +22,11 @@ function combineArray() {
 
   // 🔶 전개 구문을 사용해 spreadCombineList 배열 병합 코드를 작성하세요.
   // 참고: https://mzl.la/43TCLgA | https://mzl.la/3VTzEDh | https://mzl.la/3vC07ec
-  const spreadCombineList = [];
+  const spreadCombineList = [
+    ...countList.slice(0, 2),
+    ...numberList,
+    countList.at(-1),
+  ];
   console.log(spreadCombineList);
 
   // 아래 결과 값이 true가 나와야 합니다.
@@ -25,21 +34,28 @@ function combineArray() {
 }
 
 function combineObject() {
+  // 개발자 작성한 기본 옵션
   const defaultOptions = {
     startIndex: 0,
     loop: false,
   };
 
+  // 사용자가 설정한 커스텀 옵션
   const customOptions = {
     loop: true,
   };
 
+  // 여러 object를 병합할 수 있음
+  // 빈 객체 넣어줘야 원본을 훼손하지 않음
   const combineOptions = Object.assign({}, defaultOptions, customOptions);
   console.log(combineOptions);
 
   // 🔶 전개 구문을 사용해 spreadCombineOptions 객체 병합 코드를 작성하세요.
   // 참고: https://mzl.la/43TCLgA
-  const spreadCombineOptions = {};
+  const spreadCombineOptions = {
+    ...defaultOptions,
+    ...customOptions,
+  };
   console.log(spreadCombineOptions);
 
   // 아래 결과 값이 true가 나와야 합니다.
