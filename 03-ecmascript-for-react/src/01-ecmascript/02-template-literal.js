dfd3b5fba4530cd386ad587549313ed8f2cfb8af
seungsu-K <kss095@naver.com>
2024-07-22 15:35:00 +0900

    [학습] 템플릿 리터럴
    - 테이블 마크업하는 랜더링 함수

--- 03-ecmascript-for-react/src/01-ecmascript/02-template-literal.js
+++ 03-ecmascript-for-react/src/01-ecmascript/02-template-literal.js
@@ -1,40 +1,64 @@
 // --------------------------------------------------------------------------
 // Template literal
-// - 데이터 + 템플릿 = 마크업 스트링
+// - 데이터 + 템플릿 = 마크업 스트링 (웹 컴포넌트, 리액트)
 // - 템플릿 리터럴 구문을 사용해 마크업 스트링 생성
 // --------------------------------------------------------------------------
 
+// 데이터(상태: state)
+// 클라이언트 요청 => 서버 -> 클라이언트 응답 -> JSON
 const koreanFoods = {
-  caption: '한식 메뉴',
+  caption: "한식 메뉴",
   rows: [
-    { headline: '뚝배기 불고기', content: 8000 },
-    { headline: '스팸치즈볶음밥', content: 7500 },
-    { headline: '불고기낙지덮밥', content: 9000 },
+    { headline: "뚝배기 불고기", content: 8000 },
+    { headline: "스팸치즈볶음밥", content: 7500 },
+    { headline: "불고기낙지덮밥", content: 9000 },
   ],
 };
 
 function renderTable(data) {
   return [
     '<table class="table">',
-    '<caption class="sr-only">' + data.caption + '</caption>',
+    '<caption class="sr-only">' + data.caption + "</caption>",
     data.rows.reduce(function (htmlString, rowData) {
       const rowString = [
-        '<tr>',
-        '<th>' + rowData.headline + '</th>',
-        '<td>' + numberWithComma(rowData.content) + '원' + '</td>',
-        '</tr>',
-      ].join('');
+        "<tr>",
+        "<th>" + rowData.headline + "</th>",
+        "<td>" + numberWithComma(rowData.content) + "원" + "</td>",
+        "</tr>",
+      ].join("");
       return htmlString + rowString;
-    }, ''),
-    '</table>',
-  ].join('');
+    }, ""),
+    "</table>",
+  ].join("");
 }
 
 // 🔶 renderTableString 함수를 작성하세요.
+// JavaScript 파일 안에서 markup(구조 설계) 구성
+function renderTableString(state) {
+  const markup = `
+    <table class="table">
+      <caption class="sr-only">${state.caption}</caption>
+      ${state.rows.reduce(function (htmlString, rowItem) {
+        return (
+          htmlString +
+          `
+          <tr>
+            <th>${rowItem.headline}</th>
+            <td>${numberWithComma(rowItem.content)}원</td>
+          </tr>
+        `
+        );
+      }, "")}
+    </table>
+  `;
+  return removeSpaceHTMLString(markup);
+}
 
 function run() {
   const renderedResult = renderTable(koreanFoods);
-  return renderedResult;
+  console.log(renderedResult);
+  const renderedStringResult = renderTableString(koreanFoods);
+  return renderedStringResult;
 }
 
 console.log(run());
@@ -43,11 +67,11 @@ console.log(run());
 // utils
 
 function numberWithComma(numberValue) {
-  return numberValue.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+  return numberValue.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
 }
 
 function removeSpaceHTMLString(htmlString) {
   return htmlString.replace(/\s+<|\n|>\s+/g, function ($1) {
-    return $1.indexOf('<') > -1 ? '<' : $1.indexOf('>') > -1 ? '>' : '';
+    return $1.indexOf("<") > -1 ? "<" : $1.indexOf(">") > -1 ? ">" : "";
   });
 }
