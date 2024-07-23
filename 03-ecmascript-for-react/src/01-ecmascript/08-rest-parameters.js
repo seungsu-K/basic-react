90a0eaecda5b3bf702ac29603df28e1a6d466a50
seungsu-K <kss095@naver.com>
2024-07-23 09:35:50 +0900

    [학습] 나머지 매개변수

--- 03-ecmascript-for-react/src/01-ecmascript/08-rest-parameters.js
+++ 03-ecmascript-for-react/src/01-ecmascript/08-rest-parameters.js
@@ -2,13 +2,18 @@
 // rest parameters
 
 function _sum() {
+  //arguments는 유사 배열이기 때문에 데이터 타입을 변환 시켜줌
   const numbers = Array.from(arguments);
   return numbers.reduce((result, number) => result + number, 0);
 }
 
 // 🔶 나머지 매개변수를 사용해 sum 함수 코드 로직을 다시 작성합니다.
 // 참고: https://mzl.la/43Ro9yp
-const sum = () => {};
+
+// 화살표 함수는 arguments를 바인딩하지 않아서 나머지 매개변수를 사용하며
+// 배열이다
+const sum = (...restParams) =>
+  restParams.reduce((total, number) => total + number);
 
 let result1_1 = _sum(2, 3, 9, 12, 105);
 let result1_2 = sum(2, 3, 9, 12, 105);
