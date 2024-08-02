f0a2b2577a9d9ebd2190d1d46dc54f168950fc49
seungsu-K <kss095@naver.com>
2024-08-02 22:02:30 +0900

    [학습] 순수 함수, 부수 효과(사이드 이팩트)

--- 09-component-purity/src/pages/AvatarListPage.jsx
+++ 09-component-purity/src/pages/AvatarListPage.jsx
@@ -1,37 +1,48 @@
 // --------------------------------------------------------------------------
 // ✅ 컴포넌트 로직 재구성
 // --------------------------------------------------------------------------
-// - [ ] 데이터 분리
-// - [ ] 리스트 렌더링
+// - [x] 데이터 분리
+// - [x] 리스트 렌더링
+// - [x] 컴포넌트 속성 검사
 // - [ ] 컴포넌트 순수성 진단
 // --------------------------------------------------------------------------
-
+import { arrayOf } from 'prop-types';
 import Avatar from '@/components/Avatar';
+import { UserType } from '@/@types/globals.d';
 
-const avatarData = [
-  { name: '야무', photo: 'man-02.jpg', status: 'online' },
-  { name: '범쌤', photo: 'man-04.jpg', status: 'away' },
-  { name: '주원', photo: 'woman-04.jpg', status: 'dont-disturb' },
-  { name: '정민', photo: 'woman-01.jpg' },
-];
+const anotherData = {
+  count: 10,
+};
 
-function AvatarListPage() {
+function AvatarListPage(props) {
+  // props는 읽기 전용 데이터이므로 전달 받은 하위 컴포넌트에서 수정하면 안된다
+  // 로컬에서 복제한 뒤 데이터를 가공한다
+  const myList = [...props.list];
+
+  myList.pop();
+
+  // 리액트 렌더링 프로세스와 관련 없는 코드는 들어오면 안됨
+  // DOM 요소에 대한 접근
+  // 타이머 API
+  // let renderCount = 0;
+  // setInterval(() => {
+  //   console.log(renderCount);
+  //   document.getElementById('react-app').dataset.render = ++renderCount;
+  // }, 1000);
+
   return (
     <ul className="AvatarList">
-      <li>
-        <Avatar name="야무" photo="man-02.jpg" status="online" />
-      </li>
-      <li>
-        <Avatar name="범쌤" photo="man-04.jpg" status="away" />
-      </li>
-      <li>
-        <Avatar name="주원" photo="woman-04.jpg" status="dont-disturb" />
-      </li>
-      <li>
-        <Avatar name="정민" photo="woman-01.jpg" />
-      </li>
+      {myList.map((user) => (
+        <li key={user.id}>
+          <Avatar user={user} /> {anotherData.count++}
+        </li>
+      ))}
     </ul>
   );
 }
 
 export default AvatarListPage;
+
+AvatarListPage.propTypes = {
+  list: arrayOf(UserType).isRequired,
+};
