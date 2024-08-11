2ebbb9ebf6729a6d7db50cfb9c89ddb02c4dc2d6
seungsu-K <kss095@naver.com>
2024-08-11 14:25:35 +0900

    - History 컴포넌트
    - 상태 끌어올리기 Board -> Game
    - 게임이 진행될 때마다 변하는 squares 상태를 배열로 묶어 gameHistory 상태로 변경
    - 기존 Squares 상태는 파생된 상태인 currentSquares로 변경
    - History 컴포넌트를 게임 진행 순서마다 History list가 나오도록 구현
    - 게임 History 클릭 시 해당 History 인덱스를 렌더링하는 기능 구현

--- homework/08-09/src/tic-tac-toe/components/Board/Board.jsx
+++ homework/08-09/src/tic-tac-toe/components/Board/Board.jsx
@@ -2,43 +2,21 @@ import './Board.css';
 
 import Squares from '../Squares/Squares';
 import Status from '../Status/Status';
-import { useState } from 'react';
-import { checkWinner, INITIAL_SQUARES, PLAYER, PLAYER_COUNT } from '@/constant';
+import { arrayOf, bool, func, number, oneOf, shape } from 'prop-types';
+import { PLAYER_LIST } from '@/constant';
 
-function Board() {
-  // [상태]
-  const [squares, setSquares] = useState(INITIAL_SQUARES);
+Board.propTypes = {
+  currentPlayer: oneOf(PLAYER_LIST),
+  winnerInfo: shape({
+    winner: oneOf(PLAYER_LIST),
+    condition: arrayOf(number),
+  }),
+  isDraw: bool,
+  squares: arrayOf(oneOf(PLAYER_LIST)),
+  onPlay: func,
+};
 
-  // [상태 업데이트]
-  const handlePlay = (index) => () => {
-    // 게임이 종료되었다면 더 이상 상태 업데이트를 하지 못하도록 조건 추가
-    if (winnerInfo) {
-      alert('GAME OVER');
-      return;
-    }
-    // Square를 클릭 했을 때 해당 인덱스와 동일한 인덱스에 currnetPlayer를 넣고 상태 업데이트
-    const nextSquares = squares.map((square, squareIndex) => {
-      return index === squareIndex ? currentPlayer : square;
-    });
-
-    setSquares(nextSquares);
-  };
-
-  // [파생된 상태]
-  // 게임 순서 : Square 컴포넌트 중 null이 아닌 값들을 배열로 모아 length 반환
-  const gameIndex = squares.filter(Boolean).length;
-
-  // 현재 플레이어 : gameIndex를 플레이어 인원으로 나누어 0이면 플레이어 1 차례
-  const currentPlayer =
-    gameIndex % PLAYER_COUNT === 0 ? PLAYER.ONE : PLAYER.TWO;
-
-  // 게임 종료
-  // 1. 승리
-  const winnerInfo = checkWinner(squares);
-
-  // 2. 무승부
-  const isDraw = !winnerInfo && squares.every(Boolean);
-
+function Board({ currentPlayer, winnerInfo, isDraw, squares, onPlay }) {
   return (
     <div className="Board">
       <Status
@@ -46,7 +24,7 @@ function Board() {
         winnerInfo={winnerInfo}
         isDraw={isDraw}
       />
-      <Squares squares={squares} winnerInfo={winnerInfo} onPlay={handlePlay} />
+      <Squares squares={squares} winnerInfo={winnerInfo} onPlay={onPlay} />
     </div>
   );
 }
--- homework/08-09/src/tic-tac-toe/components/History/History.jsx
+++ homework/08-09/src/tic-tac-toe/components/History/History.jsx
@@ -1,12 +1,38 @@
 import './History.css';
 
-function History() {
+import { arrayOf, func, number, oneOf } from 'prop-types';
+import { PLAYER_LIST } from '@/constant';
+
+History.propTypes = {
+  gameHistory: arrayOf(arrayOf(oneOf(PLAYER_LIST))),
+  gameIndex: number,
+  onTimeTravel: func,
+};
+
+function History({ gameHistory, gameIndex, onTimeTravel }) {
+  const handleClick = (index) => () => {
+    onTimeTravel(index);
+  };
+
   return (
     <div className="History">
       <ol>
-        <li>
-          <button type="button">게임 시작</button>
-        </li>
+        {gameHistory.map((history, index) => {
+          const buttonLabel = index === 0 ? '게임 시작' : `게임 # ${index}`;
+          const isDisabled = gameIndex === index;
+
+          return (
+            <li key={index}>
+              <button
+                type="button"
+                disabled={isDisabled}
+                onClick={handleClick(index)}
+              >
+                {buttonLabel}
+              </button>
+            </li>
+          );
+        })}
       </ol>
     </div>
   );
